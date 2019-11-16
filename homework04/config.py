8b698feb9a4300f077ab625f2fc8cf01cae7544a
ArokhTheDragon <56166172+ArokhTheDragon@users.noreply.github.com>
2019-11-16 20:58:23 +0300

    обновлено.2.0

---
+++ homework04/config.py
@@ -0,0 +1,2 @@
+domain = 'http://www.ifmo.ru/ru/schedule/0'
+access_token = '981879027:AAH9zDV_BUrvhyZ3V6Txq7XMqHUlGsHRlbQ'
--- homework04/echo_bot.py
+++ homework04/echo_bot.py
@@ -1,12 +1,14 @@
 import telebot
 
+
 access_token = '981879027:AAH9zDV_BUrvhyZ3V6Txq7XMqHUlGsHRlbQ'
-telebot.apihelper.proxy = {'https': 'https://188.170.233.101:3128'}
+telebot.apihelper.proxy = {'https': 'https://188.170.233.105:3128'}
 
 # Создание бота с указанным токеном доступа
 bot = telebot.TeleBot(access_token)
 
 
+# Бот будет отвечать только на текстовые сообщения
 @bot.message_handler(content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
